f3dcf0d4d73eb28ea8c7d75fa0a82f68572cd682
Pratik Vyas <m@pd.io>
2013-08-28 09:14:30 +0530

    add view to read notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from .backends.web_notifications import remove_notification, get_notification
+
+def read_notification(request, notification_id):
+    notification = get_notification(request.user, notification_id)
+    remove_notification(request.user, notification_id)
+    if notification:
+        return redirect(notification['url'])
+    return redirect(reverse(settings.NOTIFICATION_READ_DEFAULT_REDIRECT))
